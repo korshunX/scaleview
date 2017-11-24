6570b59665579774ed3c85f620c2488e0e2641b9
korshunX <gourment@gmx.net>
2017-11-25 00:16:27 +0100

    restructured to a more modern approach for project structuring

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,23 @@
  */
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
-    // place code for your default task here
+    // place code for your default task here        
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+            }).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist/css'));   
+});
+   
+gulp.task('sass:watch', function () {
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
 });
